984953bcab77ee59caa3cd8d744afa9c30eb2783
Marmeladis <90033297+Marmeladis@users.noreply.github.com>
2025-05-29 22:47:02 +0300

    Delete wishwise-frontend directory

--- wishwise-frontend/src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function About() {
-  return <h1>О нас</h1>;
-}
-
-export default About;
--- wishwise-frontend/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Home() {
-  return <h1>Домашняя страница</h1>;
-}
-
-export default Home;
